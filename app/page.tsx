0d0707a037a871ccafe00a5f3f4f208625bef289
Pesta <franklinolisaemeka3@gmail.com>
2026-02-26 14:03:24 +0100

    Add TowerResearchCard component to the Home page for enhanced user insights

--- app/page.tsx
+++ app/page.tsx
@@ -18,6 +18,7 @@ export default function Home() {
 			<EarningsGuarantee />
 			<HowItWorks />
 			<CapitalProtectionStats />
+			<TowerResearchCard />
 			<LeadershipTeam />
 			<TowerResearchCard />
 		</div>
